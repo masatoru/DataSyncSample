33c8f9edb1fb44b2c404be98dc04f93832dd0a0d
Satoru Fujimori <satoru.fujimori@gmail.com>
2019-05-11 12:21:12 +0900

    Added User model

---
+++ DataSyncSample/DataSyncSample/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataSyncSample.Models
+{
+    public class User
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String PhoneNumber { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid(); //generates random id
+    }
+}
